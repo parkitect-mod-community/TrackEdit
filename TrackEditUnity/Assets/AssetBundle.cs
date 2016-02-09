4f582333ad3a352975441b1c33bc33d16b0ea543
Michael Pollind <mpollind@gmail.com>
2016-02-08 21:26:26 -0800

    added unity project

---
+++ TrackEditUnity/Assets/AssetBundle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetBundle : MonoBehaviour {
+
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles()
+	{
+		BuildPipeline.BuildAssetBundles("./../assetbundle/");
+	}
+}
