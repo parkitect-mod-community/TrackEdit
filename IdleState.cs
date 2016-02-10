10d9959e320a242f77e1e2e56f56690ef8bbfd8e
Michael Pollind <mpollind@gmail.com>
2016-02-09 16:14:34 -0800

    added ctrl then drag functionality

--- IdleState.cs
+++ IdleState.cs
@@ -5,6 +5,7 @@ namespace RollercoasterEdit
 {
     public class IdleState : IState
     {
+        private bool _verticalState= false;
         private SharedStateData _stateData;
         public IdleState ()
         {
@@ -13,7 +14,15 @@ namespace RollercoasterEdit
 
         public void Update(FiniteStateMachine stateMachine)
         {
-            var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            if (Input.GetKeyDown(Main.Configeration.VerticalKey))
+            {
+                _verticalState = true;
+            }
+            else if (Input.GetKeyUp(Main.Configeration.VerticalKey))
+            {
+                _verticalState = false;
+            }
+                var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
             if (Input.GetMouseButtonDown (0)) {
                 RaycastHit hit;
                 if (Physics.Raycast (ray, out hit, Mathf.Infinity, -1)) {
@@ -23,7 +32,12 @@ namespace RollercoasterEdit
                         _stateData.Offset = hit.transform.position - hit.point;
                         
                         _stateData.Distance = (ray.origin - hit.point).magnitude;
-            
+
+                        if (_verticalState)
+                        {
+                            stateMachine.ChangeState(new VerticalDragState(_stateData));
+                        }
+                        else
                         stateMachine.ChangeState(new HorizantalDragState(_stateData));
                     }
                 }
